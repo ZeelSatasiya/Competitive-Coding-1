f8b0338c951e1bab365302f448b8dafaf7fe6201
“ZeelSatasiya” <zeel.satasiya@gmail.com>
2024-04-03 19:36:53 -0700

    Problem 1 completed

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,17 @@
+// Find the Missing Number in a sorted array
 
+public static int missingNumber(int arr[]) {
+    int low = 0;
+    int high = arr.length - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if ((arr[mid] - mid) != (arr[0] - 0)) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return arr[low] - 1;
+}
